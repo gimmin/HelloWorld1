28ffba15d6552623c1c50fd7ffb45ba410eaef42
gimmin <kimm2570@naver.com>
2016-03-29 11:47:05 +0900

    dk

---
+++ Greetings/src/member/MemRegist.java
@@ -0,0 +1,14 @@
+package member;
+
+public class MemRegist{
+	public boolean registMem() {
+		boolean result = false;
+		return result;
+	}
+
+	public static void main(String[] args){
+	System.out.println("Hello JaVa");
+	
+	
+	}
+}
